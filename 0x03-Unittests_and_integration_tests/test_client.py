994eba52544a3d627df5441e5331067205b014d2
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2024-01-08 13:15:37 +0100

    fixed pycodestyle

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -107,7 +107,8 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def test_public_repos_with_license(self) -> None:
         """Tests the `public_repos` method with a license."""
         self.assertEqual(
-            client.GithubOrgClient("google").public_repos(license="apache-2.0"),
+            client.GithubOrgClient("google")
+            .public_repos(license="apache-2.0"),
             self.apache2_repos,
         )
 
